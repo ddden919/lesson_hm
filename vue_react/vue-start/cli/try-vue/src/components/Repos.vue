65bd6651d9c30b0937fd582bb175cc2b01fa00c5
ddden919 <1736629702@qq.com>
2025-02-04 19:26:28 +0800

    learn vue

---
+++ vue_react/vue-start/cli/try-vue/src/components/Repos.vue
@@ -0,0 +1,27 @@
+<!-- vb3s 三段式快捷方式 -->
+<template>
+     <ul>
+        <li v-for="(repo, index) in repos" :key="index">{{repo.name}}</li>
+    </ul>
+</template>
+
+<script setup>
+const repos = [
+        {
+            id: 1,
+            name:"xiaoming"
+        },
+        {
+            id: 2,
+            name:"xiaohong"
+        },
+        {
+            id: 3,
+            name:"xiaolan"
+        }
+]
+</script>
+
+<style scoped>
+
+</style>
